45b99d946ca558a17bf7261c6689b4c09a106326
Bohdan Kopanichuk <danbog2000@gmail.com>
2025-09-25 23:33:14 +0300

    Add CleanLayout component for a streamlined layout structure
    - Introduced CleanLayout.vue with a basic template for a clean layout using Quasar's layout system.
    - Implemented scoped SCSS styles to ensure full viewport height and width, enhancing the overall UI experience.
    - Added a router-view for dynamic page content rendering within the layout.

---
+++ frontend/src/layouts/CleanLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="flex clean-layout">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.clean-layout {
+  height: 100dvh;
+  width: 100dvw;
+  max-height: 100dvh;
+  max-width: 100dvw;
+}
+</style>
